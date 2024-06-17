f4e4890a50ee8514f339b1ced3c3b9984d17200b
Bart <bwj.kempen@student.avans.nl>
2024-06-17 21:45:04 +0200

    okay now works

--- Database/SeedStore.fs
+++ Database/SeedStore.fs
@@ -5,78 +5,71 @@ open StorageDatabase
 open Database.InstoreDatabase
 open Models
 
-/// Here a store is created that contains the following tables with the following attributes
-///
-/// candidates (primary key is name)
-/// - name (consists of words separated by spaces)
-/// - date of birth
-/// - guardian id (see guardian id)
-/// - highest swimming diploma (A, B, or C, with C being the highest)
-///
-/// sessions (primary key is compound: candidate name and date)
-/// - candidate name (foreign key to candidates)
-/// - date
-/// - minutes (int)
-///
-/// guardians
-/// - id (3 digits followed by dash and 4 letters, e.g. 133-LEET)
-/// - name (consists of words separated by spaces)
 
-let store = { new IStore with
+let guardians = 
+            [ ("123-ABCD", "Jan Janssen")
+              ("234-FDEG", "Marie Moor")
+              ("999-ZZZZ", "Margeet van Lankerveld") ]
+            |> Seq.choose (fun (id, name) ->
+                match Guardian.build id name with
+                | Ok guardian -> Some (GuardianId.value guardian.Id, (GuardianId.value guardian.Id, Name.value guardian.Name))
+                | _ -> None)
+            |> InMemoryDatabase.ofSeq
+            
+let candidates = 
+            [ ("Eleanor", DateTime(2016, 1, 9), "123-ABCD", "A")
+              ("Camiel", DateTime(2015, 11, 3), "123-ABCD", "C")
+              ("Lore", DateTime(2018, 8, 30), "999-ZZZZ", "") ]
+            |> Seq.choose (fun (n, bd, gi, dpl) ->
+                match Candidate.build n bd gi dpl with
+                | Ok candidate -> Some (Name.value candidate.Name, (Name.value candidate.Name, bd, GuardianId.value candidate.GuardianId, Diploma.value candidate.Diploma))
+                | Error _ -> None)
+            |> InMemoryDatabase.ofSeq
 
-    member this.candidates: InMemoryDatabase<string,(string * DateTime * string * string)> = 
-        [ ("Eleanor", DateTime(2016, 1, 9), "123-ABCD", "A")
-          ("Camiel", DateTime(2015, 11, 3), "123-ABCD", "C")
-          ("Lore", DateTime(2018, 8, 30), "999-ZZZZ", "") ]
-        |> Seq.choose (fun (n, bd, gi, dpl) ->
-            match Candidate.build n bd gi dpl with
-            | Ok candidate -> Some (Name.value candidate.Name, (Name.value candidate.Name, bd, GuardianId.value candidate.GuardianId, Diploma.value candidate.Diploma))
-            | Error _ -> None)
-        |> InMemoryDatabase.ofSeq
+let sessions = 
+            [ ("Eleanor", Shallow, DateTime(2024, 2, 2), 3)
+              ("Eleanor", Shallow, DateTime(2024, 3, 2), 5)
+              ("Eleanor", Shallow, DateTime(2024, 3, 2), 10)
+              ("Eleanor", Deep, DateTime(2024, 4, 1), 30)
+              ("Eleanor", Deep, DateTime(2024, 5, 2), 10)
+              ("Eleanor", Deep, DateTime(2024, 5, 3), 15)
+              ("Camiel", Shallow, DateTime(2023, 4, 10), 15)
+              ("Camiel", Deep, DateTime(2023, 4, 17), 10)
+              ("Camiel", Deep, DateTime(2023, 5, 24), 20)
+              ("Camiel", Deep, DateTime(2023, 5, 14), 10)
+              ("Camiel", Deep, DateTime(2023, 6, 13), 20)
+              ("Camiel", Deep, DateTime(2023, 6, 17), 10)
+              ("Camiel", Deep, DateTime(2023, 7, 10), 20)
+              ("Camiel", Deep, DateTime(2023, 7, 17), 10)
+              ("Camiel", Deep, DateTime(2023, 8, 10), 20)
+              ("Camiel", Deep, DateTime(2023, 8, 17), 10)
+              ("Camiel", Deep, DateTime(2023, 9, 10), 20)
+              ("Camiel", Deep, DateTime(2023, 9, 17), 10)
+              ("Camiel", Deep, DateTime(2023, 10, 10), 20)
+              ("Camiel", Deep, DateTime(2023, 10, 17), 10)
+              ("Camiel", Deep, DateTime(2023, 11, 10), 20)
+              ("Camiel", Deep, DateTime(2023, 11, 17), 10)
+              ("Camiel", Deep, DateTime(2023, 12, 10), 20)
+              ("Camiel", Deep, DateTime(2023, 12, 17), 10)
+              ("Lore", Shallow, DateTime(2024, 6, 3), 1)
+              ("Lore", Shallow, DateTime(2024, 6, 10), 30)
+              ("Lore", Shallow, DateTime(2024, 6, 11), 30)
+              ("Lore", Shallow, DateTime(2024, 6, 12), 30)
+              ("Lore", Shallow, DateTime(2024, 6, 13), 30) ]
+            |> Seq.choose (fun (n, pool, date, min) ->
+                match Session.build pool date min with
+                | Ok session -> Some ((n, date), (n, session.Pool, session.Date, Minutes.value session.Minutes))
+                | Error _ -> None)
+            |> InMemoryDatabase.ofSeq
 
-    member this.guardians: InMemoryDatabase<string,(string * string)> = 
-        [ ("123-ABCD", "Jan Janssen")
-          ("234-FDEG", "Marie Moor")
-          ("999-ZZZZ", "Margeet van Lankerveld") ]
-        |> Seq.choose (fun (id, name) ->
-            match Guardian.build id name with
-            | Ok guardian -> Some (GuardianId.value guardian.Id, (GuardianId.value guardian.Id, Name.value guardian.Name))
-            | _ -> None)
-        |> InMemoryDatabase.ofSeq
+type Store() =
+       interface IStore with
 
-    member this.sessions: InMemoryDatabase<(string * DateTime),(string * PoolType * DateTime * int)> = 
-        [ ("Eleanor", Shallow, DateTime(2024, 2, 2), 3)
-          ("Eleanor", Shallow, DateTime(2024, 3, 2), 5)
-          ("Eleanor", Shallow, DateTime(2024, 3, 2), 10)
-          ("Eleanor", Deep, DateTime(2024, 4, 1), 30)
-          ("Eleanor", Deep, DateTime(2024, 5, 2), 10)
-          ("Eleanor", Deep, DateTime(2024, 5, 3), 15)
-          ("Camiel", Shallow, DateTime(2023, 4, 10), 15)
-          ("Camiel", Deep, DateTime(2023, 4, 17), 10)
-          ("Camiel", Deep, DateTime(2023, 5, 24), 20)
-          ("Camiel", Deep, DateTime(2023, 5, 14), 10)
-          ("Camiel", Deep, DateTime(2023, 6, 13), 20)
-          ("Camiel", Deep, DateTime(2023, 6, 17), 10)
-          ("Camiel", Deep, DateTime(2023, 7, 10), 20)
-          ("Camiel", Deep, DateTime(2023, 7, 17), 10)
-          ("Camiel", Deep, DateTime(2023, 8, 10), 20)
-          ("Camiel", Deep, DateTime(2023, 8, 17), 10)
-          ("Camiel", Deep, DateTime(2023, 9, 10), 20)
-          ("Camiel", Deep, DateTime(2023, 9, 17), 10)
-          ("Camiel", Deep, DateTime(2023, 10, 10), 20)
-          ("Camiel", Deep, DateTime(2023, 10, 17), 10)
-          ("Camiel", Deep, DateTime(2023, 11, 10), 20)
-          ("Camiel", Deep, DateTime(2023, 11, 17), 10)
-          ("Camiel", Deep, DateTime(2023, 12, 10), 20)
-          ("Camiel", Deep, DateTime(2023, 12, 17), 10)
-          ("Lore", Shallow, DateTime(2024, 6, 3), 1)
-          ("Lore", Shallow, DateTime(2024, 6, 10), 30)
-          ("Lore", Shallow, DateTime(2024, 6, 11), 30)
-          ("Lore", Shallow, DateTime(2024, 6, 12), 30)
-          ("Lore", Shallow, DateTime(2024, 6, 13), 30) ]
-        |> Seq.choose (fun (n, pool, date, min) ->
-            match Session.build pool date min with
-            | Ok session -> Some ((n, date), (n, session.Pool, session.Date, Minutes.value session.Minutes))
-            | Error _ -> None)
-        |> InMemoryDatabase.ofSeq
-}
+        member this.candidates: InMemoryDatabase<string,(string * DateTime * string * string)> = 
+            candidates
+
+        member this.guardians: InMemoryDatabase<string,(string * string)> = 
+            guardians
+
+        member this.sessions: InMemoryDatabase<(string * DateTime),(string * PoolType * DateTime * int)> = 
+            sessions
--- Rommulbad/Program.fs
+++ Rommulbad/Program.fs
@@ -5,9 +5,11 @@ open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
 open Giraffe
 open StorageDatabase
+open Database.SeedStore
 open Services
 open Thoth.Json.Giraffe
 open Thoth.Json.Net
+open Database.SeedStore
 
 let configureApp (app: IApplicationBuilder) =
     // Add Giraffe to the ASP.NET Core pipeline
@@ -17,17 +19,17 @@ let configureServices (services: IServiceCollection) =
     // Add Giraffe dependencies
     services
         .AddGiraffe()
-        .AddSingleton<IStore>(Database.SeedStore.store)
+        .AddSingleton<IStore>(Database.SeedStore.Store())
         .AddSingleton<SessionService>(fun serviceProvider -> 
-            let store = serviceProvider.GetService<StorageDatabase.IStore>()
+            let store = serviceProvider.GetService<IStore>()
             SessionService(store)
         )
         .AddSingleton<CandidateService>(fun serviceProvider -> 
-            let store = serviceProvider.GetService<StorageDatabase.IStore>()
+            let store = serviceProvider.GetService<IStore>()
             CandidateService(store)
         )
         .AddSingleton<GuardianService>(fun serviceProvider -> 
-            let store = serviceProvider.GetService<StorageDatabase.IStore>()
+            let store = serviceProvider.GetService<IStore>()
             GuardianService(store)
         )
         .AddSingleton<Json.ISerializer>(ThothSerializer(skipNullField = false, caseStrategy = CaseStrategy.CamelCase))
